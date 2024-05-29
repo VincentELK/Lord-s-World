8cbffb02d4c435ac1f16778249e2662f48de1f01
Vincent <140976487+VincentELKHOULY@users.noreply.github.com>
2024-05-29 02:18:02 +0200

    Add files via upload

---
+++ Lords World/player.lua
@@ -0,0 +1,11 @@
+--player
+player = {}
+player.name = "Marcus"
+player.health = 100
+player.maxHealth = 100
+player.level = 1
+player.xp = 0
+player.ranking = 0
+player.inventory = {"Epee", "Potion"}
+player.location = ""
+player.gold = 1000
